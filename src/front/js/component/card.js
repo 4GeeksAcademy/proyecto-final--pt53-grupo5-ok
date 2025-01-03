4cced143eddf1b8f284c58a7f4a820ce66775768
DaniloYP <daniloyp@gmail.com>
2025-01-03 21:26:36 +0000

    se agrego foto vacia en cards

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Card = () => {
 
   return (
     <div className="container d-flex justify-content-between">
     <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src="https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -14,7 +15,7 @@ export const Card = () => {
     </div>
 
     <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src="https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -23,7 +24,7 @@ export const Card = () => {
     </div>
 
     <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src="https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
