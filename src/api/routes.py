2426f855abaef22ffabe56c66bb5d4a2c526b5e0
Patricia Lopez <89365508+plopez1985@users.noreply.github.com>
2025-01-03 22:31:38 +0000

    implementacionmetodoget

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import requests 
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,36 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/enfermedades', methods=['GET'])
+def enfermedades():
+    response = requests.get("https://odphp.health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId=30535")
+    response=response.json()
+    return jsonify({"titulo":response["Result"]["Resources"]})
+
+@api.route('/enfermedades1', methods=['GET'])
+def enfermedades1():
+    response = requests.get("https://odphp.health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId=539")
+    response=response.json()
+    return jsonify({"titulo":response["Result"]["Resources"]})
+
+
+@api.route('/enfermedades2', methods=['GET'])
+def enfermedades2():
+    response = requests.get("https://odphp.health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId=540")
+    response=response.json()
+    return jsonify({"titulo":response["Result"]["Resources"]})
+
+@api.route('/enfermedades3', methods=['GET'])
+def enfermedades3():
+    response = requests.get("https://odphp.health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId=30560")
+    response=response.json()
+    return jsonify({"titulo":response["Result"]["Resources"]})
+
+@api.route('/enfermedades4', methods=['GET'])
+def enfermedades4():
+    response = requests.get("https://odphp.health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId=30589")
+    response=response.json()
+    return jsonify({"titulo":response["Result"]["Resources"]})
+
+
