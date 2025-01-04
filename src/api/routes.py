c35f2429c66cd550ddffd992fc6ee6696e5d6bfc
Patricia Lopez <89365508+plopez1985@users.noreply.github.com>
2025-01-04 16:08:57 +0000

    actualizacion de modelos y ruta de registro

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,6 +6,7 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 import requests 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -52,5 +53,59 @@ def enfermedades4():
     response = requests.get("https://odphp.health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId=30589")
     response=response.json()
     return jsonify({"titulo":response["Result"]["Resources"]})
+
+@api.route('/registro', methods=['POST'])
+def registro():
+    email = request.json.get("email")
+    firstname = request.json.get("firstaname")
+    lastname = request.json.get("lastname")
+    birthdate = request.json.get("birthdate")
+    gender = request.json.get("gender")
+    type = request.json.get("type")
+    region = request.json.get("region")
+    password = request.json.get("password ")
+
+
+    if not email: 
+        return jsonify({"error":"email es requerido"}), 400
+    if not firstname: 
+        return jsonify({"error":"firstname es requerido"}), 400
+    if not lastname: 
+        return jsonify({"error":"lastname es requerido"}), 400
+    if not birthdate: 
+        return jsonify({"error":"birthdate es requerido"}), 400
+    if not gender: 
+        return jsonify({"error":"gender es requerido"}), 400
+    if not type: 
+        return jsonify({"error":"type es requerido"}), 400
+    if not region: 
+        return jsonify({"error":"region es requerido"}), 400
+    if not password: 
+        return jsonify({"error":"password es requerido"}), 400
+    
+    
+    encontrado = User.query.filter_by(email=email).first()
+    if encontrado: 
+        return jsonify({"error":"email es requerido"}), 400
+    
+
+    user = User()
+    user.email = email
+    user.firstname = firstname
+    user.lastname = lastname
+    user.birthdate = birthdate
+    user.gender = gender
+    user.type = type
+    user.region = region
+    user.password = generate_password_hash(password)
+
+
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"mensaje":"registro exitoso, inicie sesion por favor."}), 200
+
+
+
+
 
 
