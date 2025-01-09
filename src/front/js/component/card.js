fb27ce45c01b9f7d00fdd6554c1ae9ef4549a129
DaniloYP <daniloyp@gmail.com>
2025-01-09 23:06:45 +0000

    espacio inferior cards y botones carrusel

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -11,7 +11,7 @@ export const Card = ({ topic }) => {
     const imageUrl = topic?.ImageUrl || "https://odphp.health.gov/sites/default/files/2023-03/Screening-tests-talk-to-doc-1267603624.jpg";
 
     return (
-        <div className="container d-flex justify-content-between">
+        <div className="container d-flex justify-content-between cards-container">
             <div className="card" style={{ width: "18rem", borderRadius: "30px" }}>
                 <img src={imageUrl} className="card-img-top" alt="..." style={{ borderTopLeftRadius: "30px", borderTopRightRadius: "30px" }} />
                 <div className="card-body">
